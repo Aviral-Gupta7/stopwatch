485a4d5d6fe057c6ed53860d70cc4b0a71b1a021
Aviral-Gupta7 <ainoxg@gmail.com>
2023-12-05 18:17:19 +0530

    init

---
+++ app.js
@@ -0,0 +1,75 @@
+const play = document.querySelector(".play");
+const pause = document.querySelector(".pause");
+const reset = document.querySelector(".reset");
+const watch = document.querySelector(".watch");
+const hour = document.querySelector(".hour");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+const millisec = document.querySelector(".millisec");
+
+let one, two, three, four;
+let isPaused = false;
+
+const start = () => {
+  let sec = Number(seconds.innerText);
+  let min = Number(minutes.innerText);
+  let hou = Number(hour.innerText);
+  let ms = Number(millisec.innerText);
+  console.log(sec, min, hou, ms);
+  one = setInterval(() => {
+    if (!isPaused) {
+      if (ms === 100) {
+        ms = 0;
+      }
+      ms++;
+      millisec.innerText = ms;
+    }
+  }, 10);
+  two = setInterval(() => {
+    if (!isPaused) {
+      if (sec === 60) {
+        sec = 0;
+      }
+      sec++;
+      seconds.innerText = sec;
+    }
+  }, 1000);
+  three = setInterval(() => {
+    if (!isPaused) {
+      if (min == 60) {
+        min = 0;
+      }
+      min++;
+      minutes.innerText = min;
+    }
+  }, 60000);
+  four = setInterval(() => {
+    if (!isPaused) {
+      hou++;
+      hour.innerText = hou;
+    }
+  }, 3600000);
+  play.disabled = true;
+};
+
+play.addEventListener("click", start);
+
+pause.addEventListener("click", () => {
+  if (isPaused) {
+    isPaused = false;
+  } else {
+    isPaused = true;
+  }
+});
+
+reset.addEventListener("click", () => {
+  clearInterval(one);
+  clearInterval(two);
+  clearInterval(three);
+  clearInterval(four);
+  hour.innerText = "00";
+  seconds.innerText = "00";
+  minutes.innerText = "00";
+  millisec.innerText = "00";
+  play.disabled = false;
+});
